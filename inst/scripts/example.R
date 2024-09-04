3ba2ab744822e801eb6c73b41167cdfd01a05f16
S.Koehler <Steffen.Koehler@rub.de>
2024-09-05 01:46:35 +0200

    doc: comments in example

--- inst/scripts/example.R
+++ inst/scripts/example.R
@@ -1,11 +1,19 @@
 rm(list = ls()); devtools::load_all()
-T <- 1e3
+# create the example data
+T <- 500L # length of the time series
+table <- data.table::data.table(X = rnorm(n = T, mean = 0.5), TIME = Sys.Date() + 1:T) # simulating the (out-of-control) data as table
+
+# parametrization of the EWMA control chart
 side <- "both"
 lambda <- 0.5
 ARL <- 500L
 
+# calculate the critical value for the control limits of the EWMA chart
 h <- calculate_control_limit_ewma(lambda = lambda, ARL = ARL, fir = TRUE, side = side,
                                   replications = 5000L)
-table <- data.table::data.table(X = rnorm(n = T, mean = 0.0), TIME = Sys.Date() + 1:T)
+# apply the EWMA and append the corresponding columns
 table[, c("z", "upper_limit", "lower_limit", "exceedance") := ewma_cc(x = X, lambda = lambda, h = h, fir = TRUE, side = side)]
+head(table)
+
+# plot the control chart
 table[, plot_ewma(.SD, dates = TIME)]
